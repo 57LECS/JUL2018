7f96674cb11aa8000b8cc19cbd2f188ad321e8fb
edgardo <eddsc57@gmail.com>
2018-11-16 17:44:58 -0600

    Added packages config

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -52,6 +52,7 @@ namespace Lasallistas.iOS
             // If the application was previously in the background, optionally refresh the user interface.
         }
 
+
         public override void WillTerminate(UIApplication application)
         {
             // Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
