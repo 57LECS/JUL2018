749ff6583dc5185e4b60704dcdbd8ccf7ea18747
Jorge Quiroga <jquir38129@gmail.com>
2018-11-16 17:11:03 -0600

    Added CalendarioViewController

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 
 using UIKit;
-using Firebase.Core;
-using Firebase.Auth;
-using Firebase.CloudFirestore;
 
 namespace Lasallistas.iOS
 {
     public partial class ViewController : UIViewController
     {
         int count = 1;
-
-        Firestore db;
 
         public ViewController(IntPtr handle) : base(handle)
         {
@@ -28,39 +23,7 @@ namespace Lasallistas.iOS
                 var title = string.Format("{0} clicks!", count++);
                 Button.SetTitle(title, UIControlState.Normal);
             };
-
-            //Configurar la variable de Firestore.
-
-
-            //Base command to initialize Firebase.
-            App.Configure();
-
-            //Ejemplo para traer una colección de Firestore.
-            db.GetCollection("eventos").GetDocuments(HandleQuerySnapshotHandler);
-
-
-            App.Configure();
-
-            //Ejemplo para traer una colección de Firestore.
-            db.GetCollection("eventos").GetDocuments(HandleQuerySnapshotHandler);
-
         }
-
-        void HandleQuerySnapshotHandler(QuerySnapshot snapshot, Foundation.NSError error)
-        {
-            if (error != null) {
-
-                System.Console.WriteLine($"Error getting documents: {error.LocalizedDescription}");
-                return;
-            }
-
-            foreach (var document in snapshot?.Documents) {
-
-                System.Console.WriteLine($"{document.Id} => {document.Data}");
-            }
-                
-        }
-
 
         public override void DidReceiveMemoryWarning()
         {
